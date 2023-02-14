480d1a5a99a0510530667729fc4f32b6e4bcc50c
JuampaN <122617266+JuampaN@users.noreply.github.com>
2023-02-14 12:14:27 -0300

    Proyecto CV finalizado para Ticmas

---
+++ funciones.js
@@ -0,0 +1,34 @@
+fetch('https://randomuser.me/api/')
+    .then(response => response.json())
+    .then(data => informacionUsuario(data))
+    .catch(error => condole.log(err))   
+
+const informacionUsuario = (data) =>{
+    document.getElementById("foto").src = data.results[0].picture.large
+    document.getElementById("nombre").innerHTML=data.results[0].name.first+ " "+
+                                                data.results[0].name.last
+    document.getElementById("correo").innerHTML=data.results[0].email
+    document.getElementById("telefono").innerHTML=data.results[0].phone
+    document.getElementById("edad").innerHTML=data.results[0].dob.age
+    document.getElementById("ciudad").innerHTML=data.results[0].location.city+", "+
+                                                data.results[0].location.state+" - "+
+                                                data.results[0].location.country
+}
+
+var b = new Boolean(false);
+function mostrar_descripciones(){
+    const elementos = document.getElementsByClassName("desc");
+    if (b == false){
+        for (let i=0; i < elementos.length; i++){
+            elementos[i].style.display = "block";
+        }
+        document.getElementById("boton_mostrar").innerHTML="Ocultar descripción";
+        b = true;
+    } else {
+        for (let i=0; i < elementos.length; i++){
+            elementos[i].style.display = "none";
+        }
+        document.getElementById("boton_mostrar").innerHTML="Mostrar descripción";
+        b = false;
+    }
+};
